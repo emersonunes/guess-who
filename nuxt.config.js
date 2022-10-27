d08470838fe7d89d89d7c92847530b4bc66e82fd
Emerson Nunes <emerson.nunes@md3.pt>
2022-10-27 17:49:26 +0100

    change public to dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,6 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  buildDir: 'public/.nuxt',
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
